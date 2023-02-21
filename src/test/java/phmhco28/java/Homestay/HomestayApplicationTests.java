fd021979dc387b4d1124a02c6d5d2f61d97d33ee
PhMinhCo <cominhphamhcb@gmail.com>
2023-02-21 12:20:29 +0700

    create project

---
+++ src/test/java/phmhco28/java/Homestay/HomestayApplicationTests.java
@@ -0,0 +1,13 @@
+package phmhco28.java.Homestay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomestayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
